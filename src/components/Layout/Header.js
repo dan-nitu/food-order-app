f36ebae265a4872dfddd91a82c6025716aee038b
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-14 19:57:35 +0300

    modal state + open and close the modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,11 +4,15 @@ import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 
 const Header = (props) => {
+  const onShowCart = () => {
+    props.onShowCart();
+  };
+
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicios food!' />
