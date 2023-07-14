7b13110b477f98613dc64edc4624065a8e4318e4
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-14 16:07:34 +0300

    meal item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,7 @@
+import Card from '../UI/Card';
+
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -29,14 +33,19 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li>
-      {meal.name}, {meal.description}, {meal.price}
-    </li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,6 @@
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = () => {
+  return <form className={classes.form}>MealItemForm</form>;
+};
+export default MealItemForm;
