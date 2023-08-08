e9cbc4b09545eedadb9aa4d52006ecb3c21d09ea
Voodoo30fps <voodoo30fps@gmail.com>
2023-08-08 17:54:32 +0300

    handling errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,12 +9,20 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpsError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-http-32615-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+        // 'https://react-http-32615-default-rtdb.europe-west1.firebasedatabase.app/meals'
+        // // ^ to trigger an error
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -32,7 +40,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
@@ -41,24 +52,32 @@ const AvailableMeals = () => {
         <p>Loading...</p>
       </section>
     );
-  } else {
-    const mealsList = meals.map((meal) => (
-      <MealItem
-        id={meal.id}
-        key={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    ));
+  }
 
+  if (httpsError) {
     return (
-      <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
+      <section className={classes.MealsError}>
+        <p>{httpsError}</p>
       </section>
     );
   }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 export default AvailableMeals;
