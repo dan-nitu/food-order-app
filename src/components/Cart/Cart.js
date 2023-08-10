0cf43281bed3556a259290363b46d5cc671df5dc
Voodoo30fps <voodoo30fps@gmail.com>
2023-08-10 15:19:35 +0300

    submit and send cart data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -30,6 +30,19 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch(
+      'https://react-http-32615-default-rtdb.europe-west1.firebasedatabase.app/meals/orders.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -65,7 +78,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
 
       {!isCheckout && modalActions}
     </Modal>
